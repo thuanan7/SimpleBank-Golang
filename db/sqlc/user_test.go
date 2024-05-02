e407f7b2361aaf26f65d8f65cbe36b4ef1c774bd
root <root@ThuanAn>
2024-05-02 17:13:28 +0700

    unit testing for users table

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/thuanan7/SimpleBank-Golang/util"
+)
+
+func CreateRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: "password",
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+
+	require.True(t, user.PasswordChangedAt.IsZero())
+	require.NotZero(t, user.CreatedAt)
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	CreateRandomUser(t)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user2.Username, user1.Username)
+	require.Equal(t, user2.HashedPassword, user1.HashedPassword)
+	require.Equal(t, user2.FullName, user1.FullName)
+	require.Equal(t, user2.Email, user1.Email)
+	require.WithinDuration(t, user2.CreatedAt, user1.CreatedAt, time.Second)
+}
